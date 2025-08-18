8e5323ff0cf6614c82d4fc5ab85f0f133109e391
Helbert Gomes <helbert.gomes@justeattakeaway.com>
2025-08-18 17:11:40 -0400

    chore: add root property

--- Sources/SwiftCAD/Essentials/_Graph.swift
+++ Sources/SwiftCAD/Essentials/_Graph.swift
@@ -1,5 +1,15 @@
 import Foundation
 
+/// A structure representing a graph.
 public struct _Graph {
-    public init() {}
+
+    /// The root value of the graph.
+    let root: Any
+
+    /// Creates a new graph with the given root value.
+    /// 
+    /// - Parameter root: The root value of the graph.
+    init(_ root: Any) {
+        self.root = root
+    }
 }
