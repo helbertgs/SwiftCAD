8673c3cb2ecda86f7e7280b921667a01825ebf7a
Helbert Gomes <helbert.gomes@justeattakeaway.com>
2025-08-18 13:36:11 -0400

    feat: create trigonometric functions

---
+++ Sources/Spatial/Trigonometric Functions/Cos.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Calculates the cosine of a given angle in radians.
+/// - Parameter x: The angle in radians.
+/// - Returns: The cosine of the angle.
+public func cos(_ x: Double) -> Double {
+    return Foundation.cos(x)
+}
---
+++ Sources/Spatial/Trigonometric Functions/Sin.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Calculates the sine of a given angle in radians.
+/// - Parameter x: The angle in radians.
+/// - Returns: The sine of the angle.
+public func sin(_ x: Double) -> Double {
+    0
+}
